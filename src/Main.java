4e1b1cfc8bfc8ec5c27f832bdba1618a89e20557
ViktoriaBilevich <viktoriabilevich1@gmail.com>
2021-02-02 11:51:40 +0300

    Comments were fixed

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,4 @@ public class Main {
         ht.checkNumber();
         ht.isCircleCoverRectangle();
     }
-
 }
--- src/com/javacourse/se/lesson1/HomeTask1.java
+++ src/com/javacourse/se/lesson1/HomeTask1.java
@@ -5,43 +5,43 @@ import java.sql.SQLOutput;
 
 public class HomeTask1 {
 
-    public void convertTime(){
+    public void convertTime() {
 
         int seconds = 4500;
-        int ostSec = seconds%60;
-        int minutes = seconds/60;
+        int ostSec = seconds % 60;
+        int minutes = seconds / 60;
         System.out.println(" В 4500 секунд " + minutes + " минут " + ostSec + " секунд");
 
-        int ostMin = minutes%60;
-        int hour = minutes/60;
+        int ostMin = minutes % 60;
+        int hour = minutes / 60;
         System.out.println(" В 4500 секунд " + hour + " час " + ostMin + " минут " + ostSec + " секунд");
 
-        int ostHour = hour%24;
-        int day = hour/24;
+        int ostHour = hour % 24;
+        int day = hour / 24;
         System.out.println(" В 4500 секунд " + day + " дней " + ostHour + " час " + ostMin + " минут " + ostSec + " секунд");
 
-        int ostDay = day%7;
-        int week = day/7;
-        System.out.println(" В 4500 секунд " + week+ " недель " + ostDay + " дней " + ostHour + " час " + ostMin + " минут " + ostSec + " секунд");
+        int ostDay = day % 7;
+        int week = day / 7;
+        System.out.println(" В 4500 секунд " + week + " недель " + ostDay + " дней " + ostHour + " час " + ostMin + " минут " + ostSec + " секунд");
     }
 
-    public void reverseNumber(){
+    public void reverseNumber() {
         int n = 4125;
-        int ed = n%10;
-        int des = (n%100-ed)/10;
-        int sot = (n%1000-des)/100;
-        int tis = (n%10000-sot)/1000;
-        int result = ed*1000 + des*100 + sot*10 + tis;
+        int units = n % 10;
+        int tens = (n % 100 - units) / 10;
+        int hundreds = (n % 1000 - tens) / 100;
+        int thousand = (n % 10000 - hundreds) / 1000;
+        int result = units * 1000 + tens * 100 + hundreds * 10 + thousand;
         System.out.println("Обратное число числа " + n + " является число" + result);
     }
 
-    public void compareNumber(){
-        int n = 5863;
-        int ed = n%10;
-        int des = (n%100-ed)/10;
-        int sot = (n%1000-des)/100;
-        int tis = (n%10000-sot)/1000;
-        if(ed != des && ed != sot && ed != tis && des!=sot && des != tis && sot !=tis) {
+    public void compareNumber() {
+        int n = 5363;
+        int units = n % 10;
+        int tens = (n % 100 - units) / 10;
+        int hundreds = (n % 1000 - tens) / 100;
+        int thousand = (n % 10000 - hundreds) / 1000;
+        if (units != tens && units != hundreds && units != thousand && tens != hundreds && tens != thousand && hundreds != thousand) {
             System.out.println("Все цифры числа " + n + " различны");
         } else {
             System.out.println("Все цифры числа " + n + " не различны");
@@ -50,29 +50,27 @@ public class HomeTask1 {
 
     public void checkNumber() {
         int n = 871;
-        int sot = n/100;
-        int ed = n%10;
-        int chetn = ed%2;
+        int hundreds = n / 100;
+        int units = n % 10;
+        int evenNum = units % 2;
 
-        if (sot<10 && sot>0){
+        if (hundreds < 10 && hundreds > 0) {
             System.out.println("Число " + n + " является трехзначным");
-        }
-        else {
+        } else {
             System.out.println("Число " + n + " не является трехзначным");
         }
 
-        if (ed==7){
+        if (units == 7) {
             System.out.println("Последняя цифра числа " + n + " 7");
-        }
-        else {
+        } else {
             System.out.println("Последняя цифра числа " + n + " не 7");
         }
 
-        if (chetn==0){
+        if (evenNum == 0) {
             System.out.println("Число " + n + " является чётным");
+        } else {
+            System.out.println("Число " + n + " не является чётным");
         }
-        else {
-            System.out.println("Число " + n + " не является чётным");}
     }
 
     public void isCircleCoverRectangle() {
